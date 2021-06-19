c79924e5b0fa1a2f3c3b9b5464c7be4f13772c94
Jakub Podolski <60115575+bebsimax@users.noreply.github.com>
2021-06-19 08:19:13 +0200

    first user story

---
+++ tests/functional_tests.py
@@ -0,0 +1,11 @@
+# Jan is a careful developer who prefers logging over print statements
+# He has heard about new custom logger called ProfilLogger
+# He quickly imports it inside his working file
+# Jan creates instance of ProfilLogger
+# Before logging his current work he decides to test the new logger
+# But, he has no idea how it works
+# He checks the docstring
+# It contains information about the class and it's methods
+# Happy with the new lecture Jan starts to read
+# He finds info about debug method
+# Jan decides to try it on his new object
