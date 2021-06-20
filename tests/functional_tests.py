f7f6c8d1f6b29b1b4ae666336660767480034283
Jakub Podolski <60115575+bebsimax@users.noreply.github.com>
2021-06-20 13:33:02 +0200

    added relative import, to run tests from terminal use 'python -m tests.functional_tests'

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,11 +1,32 @@
-# Jan is a careful developer who prefers logging over print statements
-# He has heard about new custom logger called ProfilLogger
-# He quickly imports it inside his working file
-# Jan creates instance of ProfilLogger
-# Before logging his current work he decides to test the new logger
-# But, he has no idea how it works
-# He checks the docstring
-# It contains information about the class and it's methods
-# Happy with the new lecture Jan starts to read
-# He finds info about debug method
-# Jan decides to try it on his new object
+import unittest
+
+class NewUserTest(unittest.TestCase):
+
+    def test_smoke_test(self):
+        self.assertEqual(2, 3)
+
+    def test_user_can_create_logger(self):
+        # Jan is a careful developer who prefers logging over print statements
+        # He has heard about new custom logger called ProfilLogger
+        # He quickly imports it inside his working file
+        from src.zad_rek.ProfilLogger import ProfilLogger
+        # Jan creates instance of ProfilLogger called my_logger
+        my_logger = ProfilLogger()
+        a = 12
+        self.assertIsInstance(my_logger, ProfilLogger)
+    # Before logging his current work he decides to test the new logger
+    # But, he has no idea how it works
+    # He checks the docstring
+    # It contains information about the class and it's methods
+    # Happy with the new lecture Jan starts to read
+    # He finds about the info method
+    # Jan decides to try it on his new object
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
+
+
+
+
