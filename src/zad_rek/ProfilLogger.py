a4e4aa5616160aaab085fe0300f2e4f7699a1b6b
Jakub Podolski <60115575+bebsimax@users.noreply.github.com>
2021-06-21 10:01:27 +0200

    added default log_level = "WARNING"

--- src/zad_rek/ProfilLogger.py
+++ src/zad_rek/ProfilLogger.py
@@ -1,8 +1,9 @@
 import os
 class ProfilLogger:
     """Stop it, get some help"""
+    def __init__(self):
+        self.log_level = "WARNING"
 
     def info(self, msg):
         open("log.log", "a")
-
 
