0bae45690d4d0ff5c796a499dc086c9717c73286
Jakub Podolski <60115575+bebsimax@users.noreply.github.com>
2021-06-22 10:13:22 +0200

    added rest of the log levels

--- src/zad_rek/ProfilLogger.py
+++ src/zad_rek/ProfilLogger.py
@@ -6,10 +6,25 @@ class ProfilLogger:
 
     def __init__(self):
         self.levels = {
+            "debug": 10,
             "info": 20,
-            "warning": 30
+            "warning": 30,
+            "error": 40,
+            "critical": 50
         }
         self.log_level = "warning"
+
+    def set_log_level(self, level):
+        if level in self.levels.keys():
+            self.log_level = level
+
+    def debug(self, msg):
+        """Creates debug log with specified message in log.log file"""
+        method_name = inspect.currentframe().f_code.co_name
+        file = open("log.log", "a")
+        if self.levels[method_name] >= self.levels[self.log_level]:
+            file.write(msg + '\n')
+        file.close()
 
     def info(self, msg):
         """Creates info log with specified message in log.log file"""
@@ -19,14 +34,24 @@ class ProfilLogger:
             file.write(msg + '\n')
         file.close()
 
-
-    def set_log_level(self, level):
-        if level in self.levels.keys():
-            self.log_level = level
-
-
     def warning(self, msg):
         """Creates warning log with specified message in log.log file"""
+        method_name = inspect.currentframe().f_code.co_name
+        file = open("log.log", "a")
+        if self.levels[method_name] >= self.levels[self.log_level]:
+            file.write(msg + '\n')
+        file.close()
+
+    def error(self, msg):
+        """Creates error log with specified message in log.log file"""
+        method_name = inspect.currentframe().f_code.co_name
+        file = open("log.log", "a")
+        if self.levels[method_name] >= self.levels[self.log_level]:
+            file.write(msg + '\n')
+        file.close()
+
+    def critical(self, msg):
+        """Creates error log with specified message in log.log file"""
         method_name = inspect.currentframe().f_code.co_name
         file = open("log.log", "a")
         if self.levels[method_name] >= self.levels[self.log_level]:
