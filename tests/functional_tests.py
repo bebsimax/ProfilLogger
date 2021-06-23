55653371da079410af385ba8f923b09470d8bc36
Jakub Podolski <60115575+bebsimax@users.noreply.github.com>
2021-06-23 19:51:35 +0200

    extended user story

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -12,7 +12,7 @@ src_path = (os.path.join(os.path.dirname(CUR_DIR), 'src'))
 zad_rek_path = os.path.join(src_path, 'zad_rek')
 sys.path.append(zad_rek_path)
 
-from ProfilLogger import ProfilLogger, FileHandler, LogEntry
+from ProfilLogger import ProfilLogger, FileHandler, LogEntry, ProfilLoggerReader
 
 
 class UsageTest(unittest.TestCase):
@@ -38,11 +38,8 @@ class UsageTest(unittest.TestCase):
         # He finds about the info method
         # Jan decides to try it on his new object
         my_logger.info("My first info message")
-
         dir_path = os.path.dirname(os.path.realpath(__file__))
         files = os.listdir(dir_path)
-
-
         # The operation didn't cause error, and he didn't notice any new file in his working directory
         self.assertTrue("log.txt" not in files,
                         "Info method created empty log.txt file")
@@ -68,22 +65,39 @@ class UsageTest(unittest.TestCase):
         with open ("log.txt", "r") as file:
             whole_text = file.read()
             lines = whole_text.splitlines()
-            print(lines)
             self.assertTrue("My first info message" in lines[0],
                             "info didn't save message to file")
 
-        # He adds warning level message to his log
+        # Jan adds warning level message to his log
         my_logger.warning("My first warning message")
 
         # He checks if it created a new entry in his log.txt file
         with open("log.txt", "r") as file:
             whole_text = file.read()
-            print(whole_text)
             lines = whole_text.splitlines()
             self.assertTrue("My first warning message" in lines[1],
                              "warning didn't save message to file")
 
-        self.fail("this is not ever")
+    def test_ProfilLoggerReader_returns_LogEntries(self):
+        # Jan is satisfied with loggers work so far, but manually checking files made him dizzy
+        # So he creates instance of FileHandler
+        my_file_handler = FileHandler("my_log.txt")
+        # He passes it as an argument to the ProlifLogger, and quickly creates some logs
+        my_logger = ProfilLogger(handlers=my_file_handler)
+        my_logger.set_log_level("debug")
+        my_logger.debug("This is debug message")
+        my_logger.info("This is info message")
+        my_logger.warning("This is warning message")
+        my_logger.error("This is error message")
+        my_logger.critical("This is critical message")
+        # Now he creates his ProfilLoggerReader instance
+        my_file_reader = ProfilLoggerReader(handler=my_file_handler)
+        # After that, he checks if method "find_by_text" returns his logs as a list of LogsEntries
+        log_list = my_file_reader.find_by_text("message")
+        for log in log_list:
+            self.assertTrue(isinstance(log, LogEntry))
+
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
