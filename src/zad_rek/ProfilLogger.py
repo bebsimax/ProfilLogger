39ab7388bf8a8613fd0b36e90d660968b6d6291b
Jakub Podolski <60115575+bebsimax@users.noreply.github.com>
2021-06-23 09:12:01 +0200

    Added FileHandler and LogEntry

--- src/zad_rek/ProfilLogger.py
+++ src/zad_rek/ProfilLogger.py
@@ -57,3 +57,29 @@ class ProfilLogger:
         if self.levels[method_name] >= self.levels[self.log_level]:
             file.write(msg + '\n')
         file.close()
+
+
+class FileHandler:
+    """Used to save LogEntry to a .txt file"""
+    def __init__(self, file_name="log.txt"):
+        self.file_name = file_name
+
+    def save(self, log_entry):
+        """Saves given LogEntry to a txt file
+        separates date, level, msg with ;
+        if LogEntry.msg contains ; it is replaced with :"""
+
+        log_entry.msg.replace(";", ":")
+        with open (self.file_name, "a", newline="\n") as file:
+            file.write(f"{log_entry.date} ; {log_entry.level} ; {log_entry.msg}")
+
+
+class LogEntry:
+    """Creates log entries"""
+    def __init__(self, msg, level=None):
+        from datetime import datetime
+        self.msg = msg
+        if level:
+            self.level = level
+        now = datetime.now()
+        self.date = now.strftime("%b %d %Y %H:%M:%S")
