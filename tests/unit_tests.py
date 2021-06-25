d736d0362311d6ca4d7f9553820c049dc4cf13a1
Jakub Podolski <60115575+bebsimax@users.noreply.github.com>
2021-06-25 16:42:00 +0200

    added CSVHandlerTests, making separate classes ProfilLoggerTest, ProfilLoggerReader for each Handler

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -4,16 +4,41 @@ import sys
 import random
 import datetime
 import re
+import csv
 os.chdir(os.path.dirname(__file__))
 CUR_DIR = os.getcwd()
 src_path = (os.path.join(os.path.dirname(CUR_DIR), 'src'))
 zad_rek_path = os.path.join(src_path, 'zad_rek')
 sys.path.append(zad_rek_path)
 
-from ProfilLogger import ProfilLogger, FileHandler, LogEntry, ProfilLoggerReader
+from ProfilLogger import ProfilLogger, FileHandler, LogEntry, ProfilLoggerReader, CSVHandler
 
 
 class ProfilLoggerTest(unittest.TestCase):
+
+    def test_logger_has_default_warning_level(self):
+        logger = ProfilLogger(handlers=[FileHandler()])
+        self.assertEqual(logger.log_level, "warning",
+                         "warning is not default log level")
+
+    def test_set_log_level_method_changes_log_level(self):
+        logger = ProfilLogger(handlers=[FileHandler()])
+        logger.set_log_level("info")
+        self.assertEqual(logger.log_level, "info",
+                         "set_log_method didn't change log level")
+
+    def test_set_log_level_method_does_not_change_level_when_input_is_not_known(self):
+        logger = ProfilLogger(handlers=[FileHandler()])
+        logger.set_log_level("WE DO IT LIVE")
+        self.assertEqual(logger.log_level, "warning",
+                         "set_log_method changed log level after receiving invalid input")
+
+    def test_logger_raises_TypeError_when_passed_wrong_type_as_Handler(self):
+        with self.assertRaises(TypeError):
+            ProfilLogger(handlers=[11])
+            ProfilLogger(handlers=["log.txt"])
+
+class ProfilLoggerFileHandlerTest(unittest.TestCase):
 
     def setUp(self):
         global logger
@@ -22,35 +47,19 @@ class ProfilLoggerTest(unittest.TestCase):
     def tearDown(self):
         try:
             os.remove('log.txt')
-        except OSError as error:
-            #print(error)
-            #print("log.log NOT REMOVED")
+        except OSError:
             pass
-
-    def test_logger_has_default_warning_level(self):
-        self.assertEqual(logger.log_level, "warning",
-                         "warning is not default log level")
-
-    def test_set_log_level_method_changes_log_level(self):
-        logger.set_log_level("info")
-        self.assertEqual(logger.log_level, "info",
-                         "set_log_method didn't change log level")
-
-    def test_set_log_level_method_does_not_change_level_when_input_is_not_known(self):
-        logger.set_log_level("WE DO IT LIVE")
-        self.assertEqual(logger.log_level, "warning",
-                         "set_log_method changed log level after receiving invalid input")
 
     def test_logger_can_save_message_to_file(self):
         logger.warning("This is your last warning")
         dir_path = os.path.dirname(os.path.realpath(__file__))
         files = os.listdir(dir_path)
         self.assertIn("log.txt", files,
-                      "Warning method didn't create log.log file")
+                      "Warning method didn't create log.txt file")
         with open("log.txt", "r", newline="\n") as file:
             file_content = file.read()
             lines = file_content.splitlines()
-            self.assertTrue( "This is your last warning" in lines[0],
+            self.assertTrue("This is your last warning" in lines[0],
                             "warning didn't save message")
 
     def test_logger_saves_all_log_levels_that_he_needs_to_save(self):
@@ -88,11 +97,6 @@ class ProfilLoggerTest(unittest.TestCase):
                     if f"This is test message for {method}" in line:
                         self.fail(f"{method} created log when it shouldn't")
 
-    def test_logger_raises_TypeError_when_passed_wrong_type_as_Handler(self):
-        with self.assertRaises(TypeError):
-            ProfilLogger(handlers=[11])
-            ProfilLogger(handlers=["log.txt"])
-
     def test_logger_can_save_to_two_txt_files_at_once(self):
         import datetime
         first_handler = FileHandler("first.txt")
@@ -125,6 +129,97 @@ class ProfilLoggerTest(unittest.TestCase):
             os.remove('second.txt')
         except OSError:
             pass
+
+
+class ProfilLoggerCSVHandlerTest(unittest.TestCase):
+
+    def setUp(self):
+        global logger
+        logger = ProfilLogger(handlers=[CSVHandler()])
+
+    def tearDown(self):
+        try:
+            os.remove('log.csv')
+        except OSError:
+            pass
+
+    def test_logger_can_save_message_to_csv_file(self):
+        logger.warning("This is your last warning")
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        files = os.listdir(dir_path)
+        self.assertIn("log.csv", files,
+                      "Warning method didn't create log.csv file")
+        with open("log.csv", "r") as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for row in csv_reader:
+                self.assertTrue("This is your last warning" in row[2],
+                                "didn't save message to csv file")
+
+    def test_logger_saves_all_log_levels_that_he_needs_to_save(self):
+        levels = list(logger.levels.keys())
+        selected_level = random.choice(levels)
+        logger.set_log_level(selected_level)
+        levels_to_write = [name for name in levels if logger.levels[selected_level] <= logger.levels[name]]
+        for method in levels:
+            class_method = getattr(logger, method)
+            class_method(f"This is test message for {method}")
+        with open("log.csv", "r") as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for method in levels_to_write:
+                for row in csv_reader:
+                    if f"This is test message for {method}" in row[2]:
+                        break
+                else:
+                    self.fail(f"{method} didn't create a sample text")
+
+    def test_logger_does_not_save_logs_that_do_not_need_to_be_saved(self):
+        levels = list(logger.levels.keys())
+        selected_level = random.choice(levels)
+        logger.set_log_level(selected_level)
+        levels_to_write = [name for name in levels if logger.levels[selected_level] <= logger.levels[name]]
+        for method in levels:
+            class_method = getattr(logger, method)
+            class_method(f"This is test message for {method}")
+        with open("log.csv", "r") as csv_file:
+            csv_reader = csv.reader(csv_file)
+            levels_not_to_write = set(levels) - set(levels_to_write)
+            for method in levels_not_to_write:
+                for row in csv_reader:
+                    if f"This is test message for {method}" in row[2]:
+                        self.fail(f"{method} created log when it shouldn't")
+
+    def test_logger_can_save_to_two_csv_files_at_once(self):
+        import datetime
+        first_handler = CSVHandler("first.csv")
+        second_handler = CSVHandler("second.csv")
+        my_logger = ProfilLogger(handlers=[first_handler, second_handler])
+        now = datetime.datetime.now()
+        my_logger.warning("Are you still there?")
+        formatted_now = now.strftime("%d %b %Y %H:%M:%S")
+        with open("first.csv", "r") as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for row in csv_reader:
+                line_in_first = row
+                self.assertEqual(f"{row[0]},{row[1]},{row[2]}", f"{formatted_now},warning,Are you still there?",
+                            "Warning didn't save message to the first.csv file")
+        with open("second.csv", "r") as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for row in csv_reader:
+                line_in_second = row
+                self.assertEqual(f"{row[0]},{row[1]},{row[2]}", f"{formatted_now},warning,Are you still there?",
+                                 "Warning didn't save message to the second.csv file")
+
+                self.assertEqual(line_in_first, line_in_second,
+                                "Saved lines aren't equal")
+        try:
+            os.remove('first.csv')
+        except OSError:
+            pass
+        try:
+            os.remove('second.csv')
+        except OSError:
+            pass
+
 
 
 class FileHandlerTest(unittest.TestCase):
@@ -245,6 +340,112 @@ class FileHandlerTest(unittest.TestCase):
                 os.remove('test_log.txt')
             except OSError as error:
                 print(error)
+
+
+class CSVHandlerTest(unittest.TestCase):
+
+    def setUp(self):
+        global csv_handler
+        csv_handler = CSVHandler()
+
+    def tearDown(self):
+        try:
+            os.remove('log.csv')
+        except OSError:
+            pass
+        try:
+            os.remove('sample.csv')
+        except OSError:
+            pass
+
+    def test_csv_handler_default_file_name_is_log_csv(self):
+        self.assertEqual(csv_handler.file_name, "log.csv",
+                         "log.csv is not default file name")
+
+    def test_csv_handler_saves_file_name_given_during_creation(self):
+        csv_handler = CSVHandler("logger.csv")
+        self.assertEqual(csv_handler.file_name, "logger.csv",
+                         "CSVHandler didn't change file_name during creation")
+
+    def test_csv_handler_creates_log_csv_by_default(self):
+        now = datetime.datetime.now()
+        my_log = LogEntry("this is message", "this is level")
+        formatted_now = now.strftime("%d %b %Y %H:%M:%S")
+        csv_handler.save(my_log)
+        with open("log.csv", "r", newline="\n") as csv_file:
+            file_content = csv_file.read()
+            lines = file_content.splitlines()
+            lines = [component for component in lines[0].split(",")]
+            date, level, msg = tuple(lines)
+
+        self.assertEqual(date, formatted_now,
+                         "CSVHandler didn't save date")
+        self.assertEqual(msg, "this is message",
+                         "CSVHandler didn't save msg")
+        self.assertEqual(level, "this is level",
+                         "CSVHandler didn't save level")
+
+    def test_csv_handler_creates_log_with_specified_name(self):
+        now = datetime.datetime.now()
+        my_log = LogEntry("this is message", "this is level")
+        formatted_now = now.strftime("%d %b %Y %H:%M:%S")
+        csv_handler = CSVHandler("sample.csv")
+        csv_handler.save(my_log)
+        with open("sample.csv", "r", newline="\n") as file:
+            file_content = file.read()
+            lines = file_content.splitlines()
+            lines = [component.strip() for component in lines[0].split(",")]
+            date, level, msg = tuple(lines)
+
+        self.assertEqual(date, formatted_now,
+                         "CSVHandler didn't save date")
+        self.assertEqual(msg, "this is message",
+                         "CSVHandler didn't save msg")
+        self.assertEqual(level, "this is level",
+                         "CSVHandler didn't save level")
+
+    def test_csv_handler_returns_TypeError_when_passed_wrong_type(self):
+        with self.assertRaises(TypeError):
+            CSVHandler(11)
+
+    def test_csv_handler_returns_ValueError_when_input_is_too_short(self):
+        with self.assertRaises(ValueError):
+            CSVHandler("log")
+
+    def test_csv_handler_returns_ValueError_when_input_is_too_long(self):
+        with self.assertRaises(ValueError):
+            CSVHandler("logloglogloglogloglogloglogloglogloglogloglogloglogloglogloglogloglog.csv")
+
+    def test_csv_handler_returns_ValueError_when_input_does_not_end_with_dottxt(self):
+        with self.assertRaises(ValueError):
+            CSVHandler("logloglog.json")
+
+    def test_csv_handler_returns_ValueError_when_file_name_ends_with_space(self):
+        with self.assertRaises(ValueError):
+            CSVHandler("logloglog .csv")
+
+    def test_csv_handler_returns_ValueError_when_file_name_ends_with_dot(self):
+        with self.assertRaises(ValueError):
+            CSVHandler("logloglog..csv")
+
+    def test_csv_handler_returns_ValueError_when_file_contains_invalid_character(self):
+        invalid_characters = ["\\", "/", ":", "*", '"', "<", ">", "|"]
+        for invalid in invalid_characters:
+            with self.assertRaises(ValueError):
+                CSVHandler(f"loglogl{invalid}g.csv")
+
+    def test_read_method_returns_all_logs_in_form_of_LogEntries(self):
+        my_handler = CSVHandler("log.csv")
+        my_logger = ProfilLogger(handlers=[my_handler])
+        my_logger.set_log_level("debug")
+        my_logger.debug("This is debug message")
+        my_logger.info("This is info message")
+        my_logger.warning("This is warning message")
+        my_logger.error("This is error message")
+        my_logger.critical("This is critical message")
+        for log in my_handler.read():
+            self.assertTrue(isinstance(log, LogEntry),
+                            f"{log} is not an instance of LogEntry")
 
 
 class LogEntryTest(unittest.TestCase):
